a9ef8afea0d21686ac9f026a99020a1f3f02ed56
Shubham <shubham.phadtarez11@gmail.com>
2023-11-22 19:11:23 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Travel',
+  title: 'Traveller',
   description: 'Travel UI/UX App for Camping',
 }
 
